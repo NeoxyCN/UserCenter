3975136f7eb80dbc7ee68a688c042158a03800d7
Neoxy <1007050740@qq.com>
2023-07-30 22:18:00 +0800

    basic website

--- controllers/bbsController.js
+++ controllers/bbsController.js
@@ -4,9 +4,9 @@ exports.read = (req, res) => {
     var postid = req.query.id
     res.render('bbs_read', {
         user_name:"Neoxy",
-        title: "这是一篇测试帖子",
+        title: "Test post",
         post_id: postid,
-        content:"哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈sdaklfh"
+        content:"123\n233"
     })
 }
 
---
+++ views/js/comment.js
@@ -0,0 +1,4 @@
+function openCommentDialog(){
+	var url="/comment";
+	window.open (url,"comment","height=400,width=500,top=50,left=200,toolbar=no,menubar=no,location=no,status=no");
+}
